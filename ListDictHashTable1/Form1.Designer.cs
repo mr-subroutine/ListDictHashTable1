07b1db5591f9189683a112c7280983b06ab42858
mr-subroutine <darrylshack@outlook.com>
2020-06-09 21:51:38 -0700

    the user can add info to list and dictionary tables

--- ListDictHashTable1/Form1.Designer.cs
+++ ListDictHashTable1/Form1.Designer.cs
@@ -28,13 +28,176 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listView3 = new System.Windows.Forms.ListView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(45, 42);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(486, 182);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // listView2
+            // 
+            this.listView2.GridLines = true;
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(45, 288);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(486, 181);
+            this.listView2.TabIndex = 1;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(45, 230);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Add User and Grades";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "List <>";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 272);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Dictionary";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(45, 475);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 39);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Add Inventory Item";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(452, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "College Grades";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(489, 272);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Inventory";
+            // 
+            // listView3
+            // 
+            this.listView3.GridLines = true;
+            this.listView3.HideSelection = false;
+            this.listView3.Location = new System.Drawing.Point(643, 42);
+            this.listView3.Name = "listView3";
+            this.listView3.Size = new System.Drawing.Size(486, 182);
+            this.listView3.TabIndex = 8;
+            this.listView3.UseCompatibleStateImageBehavior = false;
+            this.listView3.View = System.Windows.Forms.View.Details;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(640, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "HashTable";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1092, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Books";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(643, 230);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(132, 39);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Add Book";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1172, 548);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listView3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Data Structures";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListView listView3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- ListDictHashTable1/Form1.cs
+++ ListDictHashTable1/Form1.cs
@@ -16,5 +16,182 @@ namespace ListDictHashTable1
         {
             InitializeComponent();
         }
+
+        List<string> studentGradeList = new List<string>();
+        Dictionary<string, string> myDictionary = new Dictionary<string, string>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 1 column setup
+            listView1.Columns.Add("First and Last Name", 200, HorizontalAlignment.Left);
+            listView1.Columns.Add("GPA", 350, HorizontalAlignment.Left);
+
+            // 2 column setup
+            listView2.Columns.Add("Item", 200, HorizontalAlignment.Left);
+            listView2.Columns.Add("Quantity In Stock", 350, HorizontalAlignment.Left);
+
+            // default students
+            studentGradeList.Add("Will Johson");
+            studentGradeList.Add("3.52");
+            studentGradeList.Add("Deborah Ray");
+            studentGradeList.Add("3.91");
+            studentGradeList.Add("Corey Hines");
+            studentGradeList.Add("2.71");
+            studentGradeList.Add("David Bacon");
+            studentGradeList.Add("3.10");
+
+            // default inventory items
+            myDictionary.Add("Apples", "300");
+            myDictionary.Add("Bananas", "100");
+            myDictionary.Add("Cherries", "900");
+            myDictionary.Add("Pears", "200");
+            myDictionary.Add("Oranges", "228");
+
+            //add default grades
+
+            for (int i = 0; i < studentGradeList.Count; i += 2)
+            {
+                ListViewItem students = new ListViewItem();
+                students.Text = studentGradeList[i];
+                students.SubItems.Add(studentGradeList[i + 1]);
+                listView1.Items.Add(students);
+            }
+
+            foreach (KeyValuePair<string, string> entry in myDictionary)
+            {
+                // find way to add key and its value to list item
+                ListViewItem inventory = new ListViewItem();
+                inventory.Text = entry.Key;
+                inventory.SubItems.Add(entry.Value);
+                listView2.Items.Add(inventory);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool quit = false;
+            bool nameNotEntered = true;
+            string name = "";
+            string gpa = "";
+            while (nameNotEntered == true)
+            {
+                name = Microsoft.VisualBasic.Interaction.InputBox("Please enter the first and last name:", "Enter Name (Type \"n\" to quit)");
+
+                if (name.ToLower() == "n")
+                {
+                    quit = true;
+                    break;
+                }
+
+                else if (name == "")
+                {
+                    MessageBox.Show("Please enter a first and last name");
+                }
+
+                else if (name != "")
+                {
+                    nameNotEntered = false;
+                }
+            }
+
+            bool gpaNotEntered = true;
+            while (gpaNotEntered == true)
+            {
+                gpa = Microsoft.VisualBasic.Interaction.InputBox("Please enter GPA value:", "Enter GPA (Type \"n\" to quit)");
+
+                if (quit == true)
+                {
+                    break;
+                }
+
+                else if (gpa == "")
+                {
+                    MessageBox.Show("Please enter a GPA");
+                }
+
+                else if (gpa != "")
+                {
+                    gpaNotEntered = false;
+                }
+            }
+
+            // method call to adding values to list view
+            if (quit != true)
+            {
+                addToList(name, gpa);
+            }
+        }
+
+        private void addToList(string name, string gpa, string listNum = "1")
+        {
+            if (listNum == "1")
+            {
+                ListViewItem students = new ListViewItem();
+                students.Text = name;
+                students.SubItems.Add(gpa);
+                listView1.Items.Add(students);
+            }
+            else if (listNum == "2")
+            {
+                ListViewItem stock = new ListViewItem();
+                stock.Text = name;
+                stock.SubItems.Add(gpa);
+                listView2.Items.Add(stock);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool nameNotEntered = true;
+            bool quit = false;
+            string item = "";
+            string amount = "";
+            while (nameNotEntered == true)
+            {
+                item = Microsoft.VisualBasic.Interaction.InputBox("Please enter item name", "Enter Name Type \"n\" to quit)");
+
+                if (item == "")
+                {
+                    MessageBox.Show("Please enter item name");
+                }
+
+                else if (item != "")
+                {
+                    nameNotEntered = false;
+                }
+
+                else if (item.ToLower() == "n")
+                {
+                    quit = true;
+                    break;
+                }
+            }
+
+            bool itemNotEntered = true;
+            while (itemNotEntered == true)
+            {
+                amount = Microsoft.VisualBasic.Interaction.InputBox("Please enter inventory amount", "Enter quantity (Type \"n\" to quit)");
+
+                if (quit == true)
+                {
+                    break;
+                }
+
+                else if (amount == "")
+                {
+                    MessageBox.Show("Please enter a quantity");
+                }
+
+                else if (amount != "")
+                {
+                    itemNotEntered = false;
+                    myDictionary.Add(item, amount);
+                }
+            }
+
+            string listNum = "2";
+            // method call to adding values to list view
+            addToList(item, amount, listNum);
+        }
     }
 }
